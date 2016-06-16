104fa7b4da9148e4f738fd6367747e51a32cf8ea
Michael Marner <michael@20papercups.net>
2016-06-16 14:12:09 +0930

    List route, will show all the notes.

--- mirage/config.js
+++ mirage/config.js
@@ -1,33 +1,31 @@
 export default function() {
-  export default function() {
-    this.get('/notes', function() {
-      return {
-        data: [
-          {
-            type: 'notes',
-            id: 1,
-            attributes: {
-              index: '1',
-              content: 'Remember to feed dog',
-            }
-          },
-          {
-            type: 'notes',
-            id: 2,
-            attributes: {
-              index: '2',
-              content: 'You will add tests for all this, right?',
-            }
-          },
-          {
-            type: 'notes',
-            id: 3,
-            attributes: {
-              index: '3',
-              content: 'Learn React Native at some point',
-            }
-          }]
-      };
-    });
-  }
+  this.get('/notes', function() {
+    return {
+      data: [
+        {
+          type: 'notes',
+          id: 1,
+          attributes: {
+            index: '1',
+            content: 'Remember to feed dog',
+          }
+        },
+        {
+          type: 'notes',
+          id: 2,
+          attributes: {
+            index: '2',
+            content: 'You will add tests for all this, right?',
+          }
+        },
+        {
+          type: 'notes',
+          id: 3,
+          attributes: {
+            index: '3',
+            content: 'Learn React Native at some point',
+          }
+        }]
+    };
+  });
 }
