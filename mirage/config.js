ac838712da24560b0d080224ec25d39d127eacef
Michael Marner <michael@20papercups.net>
2016-06-16 14:11:45 +0930

    Mock note data for Mirage.

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,33 @@
 export default function() {
-
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.0-beta.7/shorthands/
-  */
+  export default function() {
+    this.get('/notes', function() {
+      return {
+        data: [
+          {
+            type: 'notes',
+            id: 1,
+            attributes: {
+              index: '1',
+              content: 'Remember to feed dog',
+            }
+          },
+          {
+            type: 'notes',
+            id: 2,
+            attributes: {
+              index: '2',
+              content: 'You will add tests for all this, right?',
+            }
+          },
+          {
+            type: 'notes',
+            id: 3,
+            attributes: {
+              index: '3',
+              content: 'Learn React Native at some point',
+            }
+          }]
+      };
+    });
+  }
 }
