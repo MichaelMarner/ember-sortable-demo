0c43ca6498fc25294b480d198cb06d03d5e95a94
Michael Marner <michael@20papercups.net>
2016-06-16 14:59:27 +0930

    Update record indices when table is reordered.

--- app/components/sortable-table.js
+++ app/components/sortable-table.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateSortedOrder(indices) {
+    this.beginPropertyChanges();
+    let tracks = this.get('model').forEach((note) => {
+      var index = indices[note.get('id')];
+      if (note.get('index') !== index) {
+        note.set('index',index);
+        note.save();
+      }
+    });
+    this.endPropertyChanges();
+  },
+
   didInsertElement: function() {
-    Ember.$('.sortable').sortable();
+
+    let component = this;
+    Ember.$('.sortable').sortable({
+      update: function(e, ui) {
+        let indices = {};
+
+        $(this).children().each( (index, item) => {
+          indices[$(item).data('id')] = index+1;
+        });
+
+        component.updateSortedOrder(indices);
+      }
+    });
+
     Ember.$('.sortable').disableSelection();
   }
 });
